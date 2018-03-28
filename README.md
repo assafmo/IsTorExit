983dec4668d0c33368ea6caaade026c931327e0a
assafmo <assaf.morami@gmail.com>
2018-03-28 14:27:50 +0300

    allow to query destination ip and port

--- README.md
+++ README.md
@@ -8,9 +8,33 @@ Check if an IP is a Tor exit node.
 
 ## CLI
 
+### Synopsis
+
+```
+istorexit [--dest-ip <my_ip>] [--dest-port <my_port>] [--ip <ip_to_check>...]
+```
+
+### Install & run
+
 ```bash
-npm install -g istorexit
-istorexit [ip...]
+$ npm install -g istorexit
+$ istorexit --help
+Options:
+  --dest-ip: The target IP to which the Tor exit node communicates
+    (default: "216.58.206.110")
+  --dest-port: The target TCP port in "dest-ip" to which the Tor exit
+    node communicates
+    (default: 443)
+    (a number)
+  --ip: The IP to check if is a Tor exit node
+    (default: null)
+$ istorexit --dest-ip 185.60.216.35 --dest-port 443 --ip 1.1.1.1 --ip 1.2.1.1 --ip 1.3.1.1 --ip 1.4.1.1 --ip 1.5.1.1 --ip 104.200.20.46
+1.1.1.1 false
+1.2.1.1 false
+1.3.1.1 false
+1.4.1.1 false
+1.5.1.1 false
+104.200.20.46 true
 ```
 
 ## NodeJs
@@ -19,4 +43,10 @@ istorexit [ip...]
 const IsTorExit = require("istorexit");
 IsTorExit("104.200.20.46").then(console.log); // true
 IsTorExit("1.1.1.1").then(console.log); // false
+
+IsTorExit("104.200.20.46", "185.60.216.35").then(console.log); // true
+IsTorExit("1.1.1.1", "185.60.216.35").then(console.log); // false
+
+IsTorExit("104.200.20.46", "185.60.216.35", 80).then(console.log); // true
+IsTorExit("1.1.1.1", "185.60.216.35", 80).then(console.log); // false
 ```
--- main.js
+++ main.js
@@ -2,21 +2,30 @@
 const { promisify } = require("util");
 const lookup = promisify(require("dns").lookup);
 
+const defaultIpDest = "216.58.206.110"; // google.com
+const defaultPortDest = 443; // https
+
+function reverseIp(ip) {
+  return ip
+    .split(".")
+    .reverse()
+    .join(".");
+}
+
 // https://www.torproject.org/projects/tordnsel.html
-async function isTorExit(ip, print) {
+async function isTorExit(ipToCheckIfTor, ipDest, portDest, print) {
   let address;
   try {
     const result = await lookup(
-      `${ip
-        .split(".")
-        .reverse()
-        .join(".")}.6667.4.3.2.1.ip-port.exitlist.torproject.org`
+      `${reverseIp(ipToCheckIfTor)}.${portDest || defaultPortDest}.${reverseIp(
+        ipDest || defaultIpDest
+      )}.ip-port.exitlist.torproject.org`
     );
     address = result.address;
   } catch (e) {
     if (e.code == "ENOTFOUND") {
       if (print) {
-        console.log(ip, false);
+        console.log(ipToCheckIfTor, false);
       }
       return false;
     } else {
@@ -26,14 +35,14 @@ async function isTorExit(ip, print) {
 
   if (!address) {
     if (print) {
-      console.log(ip, false);
+      console.log(ipToCheckIfTor, false);
     }
     return false;
   }
 
   const answer = address.startsWith("127.0.0.") && address != "127.0.0.1";
   if (print) {
-    console.log(ip, answer);
+    console.log(ipToCheckIfTor, answer);
   }
   return answer;
 }
@@ -41,10 +50,28 @@ async function isTorExit(ip, print) {
 module.exports = isTorExit;
 
 if (require.main === module) {
+  const flags = require("flags");
+  flags.defineString(
+    "dest-ip",
+    defaultIpDest,
+    "The target IP to which the Tor exit node communicates"
+  );
+  flags.defineNumber(
+    "dest-port",
+    defaultPortDest,
+    'The target TCP port in "dest-ip" to which the Tor exit node communicates'
+  );
+  flags.defineMultiString("ip", null, "The IP to check if is a Tor exit node");
+  flags.parse();
+
   (async () => {
-    for (const ip of process.argv.slice(2)) {
+    for (const ip of flags.get("ip")) {
+      if (ip == null) {
+        continue;
+      }
+
       try {
-        await isTorExit(ip, true);
+        await isTorExit(ip, flags.get("dest-ip"), flags.get("dest-port"), true);
       } catch (e) {
         throw e;
       }
